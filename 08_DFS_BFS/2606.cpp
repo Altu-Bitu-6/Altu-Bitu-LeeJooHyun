da542ccffa0ce311d48e5e4f28ca17fc7e5b9657
Joo Hyun Lee <michelle8085@gmail.com>
2024-04-11 23:33:33 +0900

    [DFS, BFS]4월 11일

--- 08_DFS_BFS/2606.cpp
+++ 08_DFS_BFS/2606.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 #define MAX 100
 
-int n, m, ans;
+int n, m, ans = 0;;
 vector<vector<int>> li;
 vector<bool> visited_dfs;
 
-void dfs(int index, int ans) {
-    visited_dfs[index] = true;
-    ans++;
-
+void dfs(int index) {
     for (int i = 1; i <= n; i++) {
         if (li[index][i] == 1 && !visited_dfs[i]) {
-            dfs(i, ans);
+            // 방문했을 때 true로 바뀌도록 수정!
+            visited_dfs[i] = true;
+            // 그리고 방문 즉시 ans++
+            ans++;
+            dfs(i);
         }
     }
 }
@@ -29,14 +30,12 @@ int solution(int n, int m) {
         cin >> a >> b;
         li[a][b] = li[b][a] = 1;
     }
-    ans = 0;
-    dfs(1, ans);
+    visited_dfs[1] = true;
+    dfs(1);
     return ans;
 }
 
 int main() {
     cin >> n >> m;
-
     cout << solution(n, m);
-
 }
